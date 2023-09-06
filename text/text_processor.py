89d844fd162afcc06c42f540bb2c3025d03c1695
Grish Mahat <grishmahat@gmail.com>
2023-09-06 20:48:00 +0545

    Removed the "split_image" template

--- text/text_processor.py
+++ text/text_processor.py
@@ -1,14 +1,12 @@
 import re
 from typing import List, Dict
-from video.video_segment import VideoSegment
+from video_segment import VideoSegment
 
 
 class TextProcessor:
     TEXT_TEMPLATES = {
         "image": r"\[IMAGE:\s*(.+?)(\d*?)]",
-        "split_image": r"\[IMAGE:\s*.+?\d*?]",
         "search_voice": r"\[VOICE:\s*(.+?)](.+?)\[\/VOICE]",
-        "split_voice": r"\[VOICE:\s*.+?](.+?)\[\/VOICE]",
     }
 
     def __init__(self, text: str):
@@ -19,27 +17,24 @@ class TextProcessor:
 
     def _process_text_for_images(self) -> None:
         matches = re.finditer(TextProcessor.TEXT_TEMPLATES["image"], self.text, re.DOTALL)
-        groups = [(match.group(1), int(match.group(2)) or 5) for match in matches]
+        sentences = re.split(TextProcessor.TEXT_TEMPLATES["search_voice"], self.text)
 
-        sentences = re.split(TextProcessor.TEXT_TEMPLATES["split_image"], self.text)
-        num_sentences = len(sentences)
-        num_groups = len(groups)
-        num_segments = min(num_sentences, num_groups)
+        for i, match in enumerate(matches):
+            image_keyword = match.group(1)
+            images_number = int(match.group(2) or 5)
 
-        for i in range(num_segments):
-            sentence = sentences[i].strip()
-            voiceover_segments = self._process_voices(sentence)
-            image_keyword, images_number = groups[i]
-            video_segment = VideoSegment(sentence, voiceover_segments, image_keyword, i + 1, images_number)
-            self.video_segments.append(video_segment)
-            self.sentences.append((sentence, image_keyword))
-        
-        remaining_sentences = sentences[num_segments:]
-        # Handle the remaining sentences as needed (e.g., skip or process differently)
+            if i < len(sentences):
+                sentence = sentences[i].strip()
+                voiceover_segments = self._process_voices(sentence)
+                video_segment = VideoSegment(sentence, voiceover_segments, image_keyword, i + 1, images_number)
+                self.video_segments.append(video_segment)
+                self.sentences.append((sentence, image_keyword))
+
+        # Handle remaining sentences here (e.g., log or skip)
 
     def _process_voices(self, text: str) -> List[Dict]:
         voiceover_segments = []
-        for sentence in re.split(TextProcessor.TEXT_TEMPLATES["split_voice"], text):
+        for sentence in re.split(TextProcessor.TEXT_TEMPLATES["search_voice"], text):
             sentence = sentence.strip()
             if sentence:
                 voiceover_segments.append({"voice": "DEFAULT", "text": sentence})
